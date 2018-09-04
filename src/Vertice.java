b1d08ae4592be6d84e8b2060289078c2bf55dd5d
David Rodrigues <davidrod.contato@gmail.com>
2018-09-04 16:29:00 -0300

    Classes arestas e vertices. classe grafo ainda não estruturada

---
+++ src/Vertice.java
@@ -0,0 +1,15 @@
+public class Vertice {
+    private String nome;
+
+    public void Vertice(String nome){
+        setNome(nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
